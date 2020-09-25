6a7ad1b67577bf0c49cef0a19207bdcf8d9f87a3
sbuDiction <nsbu68501@gmail.com>
2020-09-25 11:27:57 +0200

    added procfile

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const SendMail = require('./public/modules/placeOrder');
+// Create Express app
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+
+let useSSL = false;
+const local = process.env.LOCAL || false;
+if (process.env.DATABASE_URL && !local) {
+    useSSL = true;
+}
+
+
+const email = SendMail(nodemailer);
+app.use(express.static('public'));
+
+app.post('/placeOrder', (req, res) => {
+    email.send();
+    res.send("Your order has been placed thank you we will contact you as soon as possible")
+});
+
+const PORT = process.env.PORT || 3000;
+
+
+// Start the Express server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`))
